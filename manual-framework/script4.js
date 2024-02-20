bb24c46ca320e6572e2b42c4d8e32a930e63ab25
Cittadhammo <82962905+cittadhammo@users.noreply.github.com>
2024-02-20 12:29:30 +0000

    finish data

--- manual-framework/script4.js
+++ manual-framework/script4.js
@@ -1,13 +1,6 @@
-// const fs = require('fs');
-// const path = require('path');
-// const cheerio = require('cheerio');
-// const axios = require('axios');
-
 import path from 'path';
 import cheerio from 'cheerio';
 import fs from 'fs';
-
-
 
 // Recursive function to get .htm files
 function getFiles(dir, files = []) {
@@ -18,7 +11,10 @@ function getFiles(dir, files = []) {
     if (stats.isDirectory()) {
       // If it's a directory, recursively traverse it
       getFiles(filePath, files);
-    } else if (path.extname(filePath) === '.htm') {
+    } else if (path.extname(filePath) === '.htm' 
+    && !getLastElement(filePath).startsWith('dic')
+    && !getLastElement(filePath).startsWith('table')
+    ) {
       // If it's an .htm file, add it to the list
       files.push(filePath);
     }
@@ -39,13 +35,22 @@ function extractInfoFromFile(filePath) {
     const $ = cheerio.load(html);
     const title = $('title').text();
     const bodyContent = $('body').text();
+    const title2 = $('h2').text();
+    const first = $('p:first').text();
+    const titleEn = first.trim().split(' ').slice(0,  5).join(' ')+'...';
+    const titleEn2 = first.trim().split(',')[0].replaceAll("'","");
     const links = [];
     $('a').each((i, link) => {
         const name  = getLastElement($(link).attr('href'))
-        if (!name?.startsWith('dic') && !name?.startsWith('index') && !name?.endsWith('top')) links.push(name);
+        if (!name?.startsWith('dic') 
+          && !name?.startsWith('index') 
+          && !name?.endsWith('top')
+          && !name?.startsWith('english')
+          && !name?.startsWith('table')
+          ) links.push(name);
     });
 
-    return { title, bodyContent, links };
+    return { title2, titleEn2, titleEn, title, first, links };
 }
 
 // Function to process all .htm files and output a JSON
@@ -56,15 +61,15 @@ function processFiles(filePaths) {
     });
 
     // Write the results to a JSON file
-    fs.writeFileSync('./docs/data/output.json', JSON.stringify(results, null,  2), 'utf-8');
-    console.log('Data successfully saved to output.json');
+    const location = 'data3.json'
+    const location1 = './docs/data/data.json'
+    fs.writeFileSync(location, JSON.stringify(results, null,  2), 'utf-8');
+    console.log('Data successfully saved to '+location);
 }
 
 function getLastElement(str) {
     return str?.substring(str.lastIndexOf('/') +  1);
 }
-
-
 
 // Usage
 processFiles(allFiles);
